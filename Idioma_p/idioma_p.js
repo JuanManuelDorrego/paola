cd41a71a3a607f456419c55414cae11a4bb270a4
JuanManuelDorrego <138993244+JuanManuelDorrego@users.noreply.github.com>
2024-04-17 23:52:16 -0300

    Add files via upload

---
+++ Idioma_p/idioma_p.js
@@ -0,0 +1,95 @@
+const http = require('node:http')
+const fs = require('node:fs')
+
+const mime = {
+  'html': 'text/html',
+  'css': 'text/css',
+  'jpg': 'image/jpg',
+  'ico': 'image/x-icon',
+  'mp3': 'audio/mpeg3',
+  'mp4': 'video/mp4'
+}
+
+const servidor = http.createServer((pedido, respuesta) => {
+  const url = new URL('http://localhost:8888' + pedido.url)
+  let camino = 'public' + url.pathname
+  if (camino == 'public/')
+    camino = 'public/index.html'
+  encaminar(pedido, respuesta, camino)
+})
+
+servidor.listen(8888)
+
+
+function encaminar(pedido, respuesta, camino) {
+  console.log(camino)
+  switch (camino) {
+    case 'public/recuperardatos': {
+      recuperar(pedido, respuesta)
+      break
+    }
+    default: {
+      fs.stat(camino, error => {
+        if (!error) {
+          fs.readFile(camino, (error, contenido) => {
+            if (error) {
+              respuesta.writeHead(500, { 'Content-Type': 'text/plain' })
+              respuesta.write('Error interno')
+              respuesta.end()
+            } else {
+              const vec = camino.split('.')
+              const extension = vec[vec.length - 1]
+              const mimearchivo = mime[extension]
+              respuesta.writeHead(200, { 'Content-Type': mimearchivo })
+              respuesta.write(contenido)
+              respuesta.end()
+            }
+          })
+        } else {
+          respuesta.writeHead(404, { 'Content-Type': 'text/html' })
+          respuesta.write('<!doctype html><html><head></head><body>Recurso inexistente</body></html>')
+          respuesta.end()
+        }
+      })
+    }
+  }
+}
+
+
+function recuperar(pedido, respuesta) {
+  let info = ''
+  pedido.on('data', datosparciales => {
+    info += datosparciales
+  })
+  pedido.on('end', () => {
+    const formulario = new URLSearchParams(info)
+    console.log(formulario)
+    respuesta.writeHead(200, { 'Content-Type': 'text/html' })
+
+  
+    const frase = formulario.get('frase');
+    let nuevaFrase = '';
+    const vocales = ['a', 'e', 'i', 'o', 'u'];
+    
+    for (let i = 0; i < frase.length; i++) {
+      if (vocales.includes(frase[i])) {
+        nuevaFrase += `${frase[i]}p${frase[i]}`;
+      } else {
+        nuevaFrase += frase[i];
+      }
+    }
+
+    const pagina =
+      `<!doctype html><html><head></head><body>
+      Frase original: ${formulario.get('frase')}<br>
+      Nueva frase: ${nuevaFrase}<br>
+      <a href="index.html">Retornar</a>
+      </body></html>`;
+
+    respuesta.end(pagina);
+
+  })
+}
+
+
+console.log('Servidor web iniciado')
